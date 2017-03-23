589ed959fe5ef4e666e0afd0be6c70e30a4a528a
Binara Goonawardana <binara.is@live.com>
2017-03-23 10:48:51 +0530

    added new activity page

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ angular.module('smoothflowwebsite', [
             controller: 'mainController'
         }).when('/activities', {
             templateUrl: 'pages/activities.html',
-            controller: 'mainController'
+            controller: 'activityController'
         }).when('/blog', {
             templateUrl: 'pages/blog.html',
             controller: 'mainController'
@@ -47,4 +47,79 @@ angular.module('smoothflowwebsite', [
 
     .controller('mainController', ['$scope', '$rootScope', '$location','$mdDialog', function ($scope, $rootScope, $location, $mdDialog) {
         console.log("application stated");
+    }])
+    .controller('activityController', ['$scope', function($scope) {
+        console.log("activity controller hits");
+
+        $scope.SearchKeyword = "";
+
+        var activityList = [{
+            name: 'Act-On',
+            description: 'Act-On',
+            category: 'Payment',
+            icon: '/satelite.png'
+        },{
+            name: 'Active Col',
+            description: 'Active Collabe description',
+            category: 'Database',
+            icon: '/cocktails.png'
+        },{
+            name: 'Backpack',
+            description: 'Backpack description',
+            category: 'Payment',
+            icon: '/polaroid.png'
+        },{
+            name: 'Bamboo',
+            description: 'Bamboo description',
+            category: 'Dataflow',
+            icon: '/recycling.png'
+        },{
+            name: 'Coinbase',
+            description: 'Coinbase description',
+            category: 'Database',
+            icon: '/wine.png'
+        }];
+
+        $scope.$watch('SearchKeyword', function(keyword) {
+            var _activities;
+
+            if(!keyword.length) _activities = SearchActivitiesByCategory('all');
+            else {
+                keyword = keyword.toLowerCase();
+                _activities = SearchActivitiesByName(keyword);
+            }
+
+            $scope.activities = _activities;
+            console.log($scope.activities);
+        });
+
+        $scope.toggleCategory = function(category) {
+            $scope.activities = SearchActivitiesByCategory(category);
+        }
+
+        var SearchActivitiesByName = function(name) {
+            return activityList.filter(function(activity) {
+                var activity_name = activity.name.toLowerCase();
+                return (activity_name.search(name) !== -1);
+            });
+        }
+
+        var SearchActivitiesByCategory = function(category) {
+            if(category === 'all')
+                return angular.copy(activityList);
+            else {
+                return activityList.filter(function(activity) {
+                    return (activity.category === category)
+                });
+            }
+        }
+
+        var init = function() {
+            var _categories = [];
+
+            activityList.forEach(function(activity) {
+                // if(_categories.indexOf() )
+            });
+        }
+
     }]);
