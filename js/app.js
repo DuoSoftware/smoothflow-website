05f5df3ebe4c2ab43843c9dc5630129c84ed544a
Lakmini Senevirathna <lakmini@duosoftware.com>
2017-03-28 13:37:54 +0530

    WIP priceplan

--- js/app.js
+++ js/app.js
@@ -45,10 +45,49 @@ angular.module('smoothflowwebsite', [
         }).otherwise({ redirectTo: '/pagenotfound' });
     }])
 
-    .controller('mainController', ['$scope', '$rootScope', '$location','$mdDialog', function ($scope, $rootScope, $location, $mdDialog) {
+    .controller('mainController', ['$scope', '$rootScope', '$location', '$mdDialog', '$http', function ($scope, $rootScope, $location, $mdDialog, $http) {
         console.log("application stated");
+
+
+
+        $scope.getplan = function () {
+            debugger;
+            $http({
+                method: 'GET',
+                url: 'http://dev.smoothflow.io/apis/plan/priceplan.json',
+                dataType: "json",
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+                    "Access-Control-Allow-Headers": "Content-Type, X-Requested-With",
+                    "Content-Type": "text/json"
+                },
+
+            })
+                .success(function (data) {
+                    console.log("Ok : " + data);
+                    $scope.palndetails=data;
+
+                })
+                .error(function (data) {
+
+                    console.log("Error : " + data);
+                });
+
+            // var url = "http://dev.smoothflow.io/apis/plan/priceplan.json"
+            // // $sce.trustAsResourceUrl(url);
+
+            // $http.jsonp(url)
+            //     .success(function (data, status, headers, config) {
+            //         console.log("Ok : "+data);
+            //     }).error(function (data, status, headers, config) {
+            //         console.log("Error : "+data);
+            //     });
+        };
+
+        $scope.getplan();
     }])
-    .controller('activityController', ['$scope', function($scope) {
+    .controller('activityController', ['$scope', function ($scope) {
         console.log("activity controller hits");
 
         $scope.SearchKeyword = "";
@@ -58,32 +97,32 @@ angular.module('smoothflowwebsite', [
             description: 'Act-On',
             category: 'Payment',
             icon: '/satelite.png'
-        },{
+        }, {
             name: 'Active Col',
             description: 'Active Collabe description',
             category: 'Database',
             icon: '/cocktails.png'
-        },{
+        }, {
             name: 'Backpack',
             description: 'Backpack description',
             category: 'Payment',
             icon: '/polaroid.png'
-        },{
+        }, {
             name: 'Bamboo',
             description: 'Bamboo description',
             category: 'Dataflow',
             icon: '/recycling.png'
-        },{
+        }, {
             name: 'Coinbase',
             description: 'Coinbase description',
             category: 'Database',
             icon: '/wine.png'
         }];
 
-        $scope.$watch('SearchKeyword', function(keyword) {
+        $scope.$watch('SearchKeyword', function (keyword) {
             var _activities;
 
-            if(!keyword.length) _activities = SearchActivitiesByCategory('all');
+            if (!keyword.length) _activities = SearchActivitiesByCategory('all');
             else {
                 keyword = keyword.toLowerCase();
                 _activities = SearchActivitiesByName(keyword);
@@ -93,33 +132,35 @@ angular.module('smoothflowwebsite', [
             console.log($scope.activities);
         });
 
-        $scope.toggleCategory = function(category) {
+        $scope.toggleCategory = function (category) {
             $scope.activities = SearchActivitiesByCategory(category);
         }
 
-        var SearchActivitiesByName = function(name) {
-            return activityList.filter(function(activity) {
+        var SearchActivitiesByName = function (name) {
+            return activityList.filter(function (activity) {
                 var activity_name = activity.name.toLowerCase();
                 return (activity_name.search(name) !== -1);
             });
         }
 
-        var SearchActivitiesByCategory = function(category) {
-            if(category === 'all')
+        var SearchActivitiesByCategory = function (category) {
+            if (category === 'all')
                 return angular.copy(activityList);
             else {
-                return activityList.filter(function(activity) {
+                return activityList.filter(function (activity) {
                     return (activity.category === category)
                 });
             }
         }
 
-        var init = function() {
+        var init = function () {
             var _categories = [];
 
-            activityList.forEach(function(activity) {
+            activityList.forEach(function (activity) {
                 // if(_categories.indexOf() )
             });
         }
+
+
 
     }]);
