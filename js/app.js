7dfd246f7879429d58441a7409c8dc55f6f6e6db
Lakmini Senevirathna <lakmini@duosoftware.com>
2017-05-02 14:28:25 +0530

    WIP

--- js/app.js
+++ js/app.js
@@ -175,9 +175,7 @@ angular.module('smoothflowwebsite', [
                 dataType: "json",
                 headers: {
                     "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-                    "Access-Control-Allow-Headers": "Content-Type, X-Requested-With",
-                    "Content-Type": "text/json"
+                    
                 },
 
             })
